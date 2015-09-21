995a6ea0ac99bdb20ebbf7dfcf2967d16212193b
nstovring <nstovring@live.dk>
2015-09-21 13:06:32 +0200

    Kinect unity folder

---
+++ Kinects/Assets/NetworkManager.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NetworkManager : MonoBehaviour {
+	
+	private const string typeName = "UniqueGameName";
+	private const string gameName = "P2 Networking";
+	
+	public int thisPlayer;
+	public GameObject playerPrefab;
+	public GameObject kinectPrefab;
+	public GameObject target;
+	public static int playerCount = Network.connections.Length;
+	//int state
+	public static NetworkPlayer[] players = new NetworkPlayer[4];
+	public static int playerID;
+	private HostData[] hostList;
+	
+	private void StartServer()
+	{
+		Network.InitializeServer(4, 25000, !Network.HavePublicAddress());
+		MasterServer.RegisterHost(typeName, gameName);
+	}
+	// Use this for initialization
+	void OnServerInitialized()
+	{
+		Debug.Log("Server Initializied");
+	}
+	void OnConnectedToServer()
+	{
+		
+	}
+	void OnPlayerConnected(NetworkPlayer player) {
+		playerID = int.Parse(player.ToString());
+		Debug.Log("Player " + playerID + " connected from " + player.ipAddress + ":" + player.port);
+		Debug.Log("Amount of players connected is " + Network.connections.Length);
+	}
+	[RPC]
+	void setPlayers(NetworkPlayer player, int _playerID){
+		players[_playerID -1] = player;
+	}
+	
+	void OnGUI()
+	{
+		if (!Network.isClient && !Network.isServer)
+		{
+			if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
+				StartServer();
+			
+			if (GUI.Button(new Rect(100, 250, 250, 100), "Refresh Hosts"))
+				RefreshHostList();
+			
+			if (hostList != null)
+			{
+				for (int i = 0; i < hostList.Length; i++)
+				{
+					if (GUI.Button(new Rect(400, 100 + (110 * i), 300, 100), hostList[i].gameName))
+						JoinServer(hostList[i]);
+				}
+			}
+		}
+	}
+	
+	private void RefreshHostList()
+	{
+		MasterServer.RequestHostList(typeName);
+	}
+	
+	void OnMasterServerEvent(MasterServerEvent msEvent)
+	{
+		if (msEvent == MasterServerEvent.HostListReceived)
+			hostList = MasterServer.PollHostList();
+	}
+	
+	private void JoinServer(HostData hostData)
+	{
+		Network.Connect(hostData);
+	}
+	
+}
+
---
+++ Kinects/Assets/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	KinectManager kinectManager;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		kinectManager = KinectManager.Instance;
+
+		uint playerID = kinectManager != null ? kinectManager.GetPlayer1ID() : 0;
+
+		Vector3 posPointMan = kinectManager.GetUserPosition(playerID);
+		Quaternion rotCube = kinectManager.GetUserOrientation(playerID, false);
+
+		transform.eulerAngles =  rotCube.eulerAngles;
+		transform.position = posPointMan*20;
+
+
+	}
+}
