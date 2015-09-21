c462184c60fbf4ba1285afd63c0d55b72672be94
nstovring <nstovring@live.dk>
2015-09-21 14:38:42 +0200

    k8g

--- Kinects/Assets/Player.cs
+++ Kinects/Assets/Player.cs
@@ -12,13 +12,14 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		kinectManager = KinectManager.Instance;
-
 		uint playerID = kinectManager != null ? kinectManager.GetPlayer1ID() : 0;
+		if(kinectManager.IsPlayerCalibrated(playerID)){
 
 		Vector3 posPointMan = kinectManager.GetUserPosition(playerID);
-		Quaternion rotCube = kinectManager.GetUserOrientation(playerID, false);
+		Quaternion rotCube = kinectManager.GetUserOrientation(playerID, true);
 
 		transform.eulerAngles =  rotCube.eulerAngles;
 		transform.position =  new Vector3(posPointMan.x*20,0,posPointMan.z*20);
+		}
 	}
 }
