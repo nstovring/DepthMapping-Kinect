3a623996546435829fcf1e4a1cd93a9b00ea3728
deadpanda1 <esberntk@gmail.com>
2015-09-23 12:05:22 +0200

    Slight joint update

--- Kinects/Assets/Player.cs
+++ Kinects/Assets/Player.cs
@@ -8,9 +8,23 @@ public class Player : MonoBehaviour {
 	void Start () {
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
+    public bool bothJointsTracked(uint userID, int Joint1, int Joint2)
+    {
+        if(kinectManager.IsJointTracked(userID, Joint1) && kinectManager.IsJointTracked(userID, Joint2))
+        {
+            return true;
+        }
+        return false;
+    }
+    public Vector3[] getBothJointsPos(uint userID, int Joint1, int Joint2)
+    {
+        Vector3[] output = new Vector3[2];
+        output[0] = kinectManager.GetJointPosition(userID, Joint1);
+        output[1] = kinectManager.GetJointPosition(userID, Joint2);
+        return output;
+    }
+    // Update is called once per frame
+    void Update () {
 		kinectManager = KinectManager.Instance;
 		uint playerID = kinectManager != null ? kinectManager.GetPlayer1ID() : 0;
 		Debug.Log(kinectManager.IsPlayerCalibrated(playerID));
@@ -19,6 +33,12 @@ public class Player : MonoBehaviour {
 		
 		Vector3 posPointMan = kinectManager.GetUserPosition(playerID);
 		Quaternion rotCube = kinectManager.GetUserOrientation(playerID, true);
+            Debug.Log(kinectManager.GetJointPosition(playerID, (int)KinectWrapper.NuiSkeletonPositionIndex.HandRight));
+            if(bothJointsTracked(playerID, (int)KinectWrapper.NuiSkeletonPositionIndex.HandRight, (int)KinectWrapper.NuiSkeletonPositionIndex.HandLeft))
+            {
+                Vector3[] hands = getBothJointsPos(playerID, (int)KinectWrapper.NuiSkeletonPositionIndex.HandRight, (int)KinectWrapper.NuiSkeletonPositionIndex.HandLeft);
+            }
+
 
 		transform.eulerAngles =  rotCube.eulerAngles;
 		transform.position =  new Vector3(posPointMan.x*20, posPointMan.y, posPointMan.z*20);
