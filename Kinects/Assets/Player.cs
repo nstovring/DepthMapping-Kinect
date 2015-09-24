fec444662104c2996fd19886259553edb73d0e88
nstovring <nstovring@live.dk>
2015-09-24 20:16:58 +0200

    Updated Networking
    - The hands vectors will be stored within two arrays in the server class

--- Kinects/Assets/Player.cs
+++ Kinects/Assets/Player.cs
@@ -1,20 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.Networking;
+using System;
 
-public class Player : NetworkBehaviour{
+public class Player : NetworkBehaviour {
 
-	KinectManager kinectManager;
+    KinectManager kinectManager;
     NetworkView myView;
     NetworkClient myClient;
     NetworkIdentity myIdentity;
     PlayerController myPlayer;
+    public Server server;
 
+    //public Vector3[] joints1;
+    //public Vector3[] joints2;
 
-    //[SyncList<T0>]
-    public Vector3[] joints1;
-   // [SyncList<Vector3>]
-    public Vector3[] joints2;
+    public Vector3[] joints;
     // Use this for initialization
     void Start () {
         myIdentity = GetComponent<NetworkIdentity>();
@@ -36,50 +37,103 @@ public class Player : NetworkBehaviour{
         return output;
     }
     // Update is called once per frame
+
+    public Vector3[] hands;
+
+    [Client]
     void Update()
     {
+        if ((transform.name == "" || transform.name == "Player(Clone)")) {
+            transform.name = playerUniqueIdentity;
+        }
+        CmdSetJointArray(hands, int.Parse(playerNetID.ToString()));
         kinectManager = KinectManager.Instance;
         uint playerID = kinectManager != null ? kinectManager.GetPlayer1ID() : 0;
-        Debug.Log(kinectManager.IsPlayerCalibrated(playerID));
 
         if (kinectManager.IsPlayerCalibrated(playerID))
         {
-
             Vector3 posPointMan = kinectManager.GetUserPosition(playerID);
             Quaternion rotCube = kinectManager.GetUserOrientation(playerID, true);
             if (bothJointsTracked(playerID, (int)KinectWrapper.NuiSkeletonPositionIndex.HandRight, (int)KinectWrapper.NuiSkeletonPositionIndex.HandLeft))
             {
-                // Debug.Log(kinectManager.GetJointPosition(playerID, (int)KinectWrapper.NuiSkeletonPositionIndex.HandRight));
-                if (Network.isClient)
-                {
-
-                    Vector3[] hands = getBothJointsPos(playerID, (int)KinectWrapper.NuiSkeletonPositionIndex.HandRight, (int)KinectWrapper.NuiSkeletonPositionIndex.HandLeft);
-                    Debug.Log(Network.connections.Length);
-                    
-                    CmdJointArray(hands, myIdentity.playerControllerId);
-
-                    transform.eulerAngles = rotCube.eulerAngles;
-                    transform.position = new Vector3(posPointMan.x * 20, posPointMan.y, posPointMan.z * 20);
-                }
+                hands = getBothJointsPos(playerID, (int)KinectWrapper.NuiSkeletonPositionIndex.HandRight, (int)KinectWrapper.NuiSkeletonPositionIndex.HandLeft);
+                CmdSetJointArray(hands, int.Parse(playerNetID.ToString()));
+                CmdMoveCube(posPointMan, rotCube);
+                //transform.eulerAngles = rotCube.eulerAngles;
+                //transform.position = new Vector3(posPointMan.x * 20, posPointMan.y, posPointMan.z * 20);
             }
         }
     }
 
-    void OnConnectedToServer() {
+    [SyncVar] public string playerUniqueIdentity;
+    private NetworkInstanceId playerNetID;
+
+    public override void OnStartLocalPlayer()
+    {
+        GetNetIdentity();
+        SetIdentity();
+
         Debug.Log("Im connected!");
+        print("Player ID is " + int.Parse(playerNetID.ToString()));
         //myClient = new NetworkClient();
     }
 
-    [Command]
-    public void CmdJointArray(Vector3[] joints, int index) {
+    public override void OnStartClient()
+    {
+        server = GameObject.FindGameObjectWithTag("Server").GetComponent<Server>();
+    }
 
-        if (index == -1)
-        {
-            this.joints1 = joints;
+    private void SetIdentity()
+    {
+        if (isLocalPlayer) {
+            transform.name = playerUniqueIdentity;
         }
-        if (index == 0)
+        else
         {
-            this.joints2 = joints;
+            transform.name = MakeUniqueIdentity();
         }
+    }
+   
+    private void GetNetIdentity()
+    {
+        playerNetID = GetComponent<NetworkIdentity>().netId;
+        CmdTellServerMyIdentity(MakeUniqueIdentity());
+    }
+    [Command]
+    private void CmdTellServerMyIdentity(string name)
+    {
+        playerUniqueIdentity = name;
+    }
+    private string MakeUniqueIdentity()
+    {
+        return "Player " + playerNetID.ToString();
+    }
+
+    [Command]
+    public void CmdMoveCube(Vector3 position, Quaternion Rotation) {
+        transform.eulerAngles = Rotation.eulerAngles;
+        transform.position = new Vector3(position.x, position.y, position.z);
+    }
+
+    [Command]
+    public void CmdSetJointArray(Vector3[] joints, int index) {
+        if (index == 1)
+        {
+            this.joints[0] = joints[0];
+            this.joints[1] = joints[1];
+            //this.joints[0] = new Vector3(1, 1, 1);
+            //this.joints[1] = new Vector3(2, 2, 2);
+            //this.joints1 = joints;
+        }
+        if (index == 2)
+        {
+            this.joints[0] = joints[0];
+            this.joints[1] = joints[1];
+            //this.joints[0] = new Vector3(3, 3, 3);
+            //this.joints[1] = new Vector3(4, 4, 4);
+            //this.joints2 = joints;
+        }
+
+        //ClientSetJoints(joints, index);
     }
 }
--- Kinects/Assets/Server.cs
+++ Kinects/Assets/Server.cs
@@ -1,15 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
 public class Server : MonoBehaviour {
 
+    public Vector3[] hands1;
+    public Vector3[] hands2;
+
+
+    GameObject[] players;
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+
+    public void SetJoints(Vector3[] joints, int player)
+    {
+        if (player == 1)
+        {
+            hands1 = joints;
+        }
+        if (player == 2)
+        {
+            hands2 = joints;
+        }
+    }
+
 	// Update is called once per frame
+    //[Server]
 	void Update () {
-	
-	}
+       players = GameObject.FindGameObjectsWithTag("Player");
+        Debug.Log(players.Length);
+        for( int i = 0; i< players.Length; i++) {
+            if(i == 0)
+            hands1 = players[i].GetComponent<Player>().joints;
+            if(i == 1)
+            hands2 = players[i].GetComponent<Player>().joints;
+        }
+    }
 }
