d1a466e7e2a56a60906c39c4215b9d6d4dae8370
nstovring <nstovring@live.dk>
2015-10-09 13:54:18 +0200

    Smaller box

--- Kinects/Assets/Scripts/OffsetCalculator.cs
+++ Kinects/Assets/Scripts/OffsetCalculator.cs
@@ -9,6 +9,8 @@ public class OffsetCalculator : NetworkBehaviour {
     public Vector3 offsetVectorRightHand;
     public Vector3 offsetVectorLeftHand;
 
+    public Vector3 offset;
+
     GameObject[] players;
 	void Start () {
 	
@@ -32,9 +34,11 @@ public class OffsetCalculator : NetworkBehaviour {
        players = GameObject.FindGameObjectsWithTag("Player");
 
         if (players.Length >= 2) {
-            offsetVectorRightHand = CalcOffset(hands1[0], hands2[0]);
-            offsetVectorLeftHand = CalcOffset(hands1[1], hands2[1]);
-            CmdSetOffsets(players);
+
+            offset = players[0].transform.position - players[1].transform.position;
+            //offsetVectorRightHand = CalcOffset(hands1[0], hands2[0]);
+            //offsetVectorLeftHand = CalcOffset(hands1[1], hands2[1]);
+            //CmdSetOffsets(players);
         }
         for( int i = 0; i< players.Length; i++) {
             if(i == 0)
