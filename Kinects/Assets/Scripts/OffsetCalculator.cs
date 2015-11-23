becce96cec55b1b1346b6db0df6fd3764f52e74d
deadpanda1 <esberntk@gmail.com>
2015-11-23 15:44:12 +0100

    MovementDifference

--- Kinects/Assets/Scripts/OffsetCalculator.cs
+++ Kinects/Assets/Scripts/OffsetCalculator.cs
@@ -11,9 +11,16 @@ public class OffsetCalculator : NetworkBehaviour {
     public GameObject[] players;
     private float player1AngleFromKinect;
 
-	void Start () {
-	
-	}
+
+    public Vector3[] oldCords;
+    public Vector3[] vel;
+    public float[] angles;
+
+    void Start () {
+        oldCords = new Vector3[2];
+        vel = new Vector3[2];
+        angles = new float[1];
+    }
 
 
 	// Update is called once per frame
@@ -30,20 +37,35 @@ public class OffsetCalculator : NetworkBehaviour {
             player2angleOffset = player1AngleFromKinect + Mathf.Abs(player2Controller.angleFromKinect) + Mathf.Abs(player2Controller.angleBetweenKinects);
             SetPositionOffset();
             SetRotationOffset();
+            //MovementDiff();
         }
 	}
     [Server]
     private void SetRotationOffset()
     {
         //players[1].GetComponent<CubeController>().YRotationOffset = 
-            Debug.Log(Vector3.Angle(players[0].transform.forward,
-            players[0].transform.forward));
+            //Debug.Log(Vector3.Angle(players[0].transform.forward,
+            //players[1].transform.forward));
     }
     [Server]
     private void SetPositionOffset()
     {
         players[1].GetComponent<CubeController>().positionOffset = this.player2Offset;
         players[1].GetComponent<CubeController>().otherAngleFromKinect = this.player1AngleFromKinect;
+    }
+    [Server]
+    private void MovementDiff()
+    {
+        for(int i = 0; i < players.Length; i++)
+        {
+            vel[i] = oldCords[i] - players[i].transform.position;
+            oldCords[i] = players[i].transform.position;
+        }
+        for(int i = 1; i < vel.Length; i++)
+        {
+            angles[i - 1] = Vector3.Angle(vel[0], vel[i]);
+            Debug.Log(angles[i - 1]);
+        }
     }
 
 }
